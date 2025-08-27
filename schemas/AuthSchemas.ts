72e0b422e490add613d5081b425471162a2f3319
Quentin Vespero <quentinvespero@gmail.com>
2025-08-28 01:41:03 +0200

    add error messages for fields email and password

--- schemas/AuthSchemas.ts
+++ schemas/AuthSchemas.ts
@@ -1,6 +1,8 @@
 import z from "zod"
 
 export const LoginSchema = z.object({
-    email: z.email(),
-    password: z.string()
+    email: z.email({error:'email is required'}),
+
+    // password should be at least 1 character
+    password: z.string().min(1,{error:'password is required'})
 })
