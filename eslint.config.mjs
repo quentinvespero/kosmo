a423c35a4b17092465240bd01290b75380e0266c
Quentin Vespero <quentinvespero@gmail.com>
2026-01-23 18:20:13 +0100

    changed target  for ES2022, update all packages, add rules for semi colon in linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      'semi': ['error', 'never']
+      'semi': ['warn', 'always'],
+      '@typescript-eslint/semi': ['warn', 'never']
     }
   }
 ]
