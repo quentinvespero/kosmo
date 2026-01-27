74af0a87b401492e89744c10c158987569335e72
Quentin Vespero <quentinvespero@gmail.com>
2026-01-27 20:58:07 +0100

    solved issue to generate tables via better auth by creating the prisma client using an adapter in auth.ts + removed custom output for schema.prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,13 @@
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
-import { PrismaClient } from "@/generated/prisma/client"
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
 
-const prisma = new PrismaClient()
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!
+})
+
+const prisma = new PrismaClient({ adapter })
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, { provider: "postgresql" })
