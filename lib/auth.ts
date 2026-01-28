cfb3bd3964e9f66b5644c01ff802b950f5e9e3cd
Quentin Vespero <quentinvespero@gmail.com>
2026-01-28 20:18:21 +0100

    finishing better-auth installation

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import { PrismaClient } from "@prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
+import { nextCookies } from "better-auth/next-js"
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!
@@ -11,5 +12,12 @@ const prisma = new PrismaClient({ adapter })
 
 export const auth = betterAuth({
     emailAndPassword: { enabled: true },
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 120 // 2 minutes caches
+        }
+    },
+    plugins: [nextCookies()],
     database: prismaAdapter(prisma, { provider: "postgresql" })
 })
