787da4260154bcfbca50d659dbe446d192bca59c
Quentin Vespero <quentinvespero@gmail.com>
2026-02-02 19:45:44 +0100

    add shadcn tabs component and use it in login page.tsx

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,16 @@
 import { LoginForm } from '@/components/auth/loginForm'
-import React from 'react'
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 const LoginPage = () => {
     return (
         <div className='loginPage'>
-            <LoginForm/>
+            <LoginForm />
+            <Tabs defaultValue='signing' className='max-auto w-full py-6 px-4'>
+                <TabsList>
+                    <TabsTrigger value='signin'>Sign In</TabsTrigger>
+                    <TabsTrigger value='signup'>Sign Up</TabsTrigger>
+                </TabsList>
+            </Tabs>
         </div>
     )
 }
