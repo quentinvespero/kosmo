cff86c9da98e7da2ae6bcf492f5b142cb21fce07
Quentin Vespero <quentinvespero@gmail.com>
2026-02-11 21:42:12 +0100

    remove workflow with the cookie session that is gonna be handled by the middleware/proxy

--- components/home/HomeContent.tsx
+++ components/home/HomeContent.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { signOut, useSession } from "@/lib/authClient"
+import { useSession } from "@/lib/authClient"
 import ShowUsers from "../testingComponents/ShowUsers"
-import Link from "next/link"
 import ShowUserDetails from "../testingComponents/ShowUserDetails"
-import { Button } from "../ui/button"
 
 type User = { id: string; name: string | null }
 
@@ -17,31 +15,10 @@ const HomeContent = ({ users }: { users: User[] }) => {
     }
 
     return (
-        <div className="flex flex-col gap-5">
-            {data == null
-                ? (
-                    <div className="flex flex-col gap-5 items-center">
-                        <p>Not connected</p>
-                        <Button className="flex">
-                            <Link href='/login' className='font-bold p-5 rounded-md'>Login / Register</Link>
-                        </Button>
-                    </div>
-
-                )
-                : (
-                    <>
-                        <Button
-                            size='sm'
-                            variant='destructive'
-                            onClick={() => signOut()}
-                        >
-                            sign out
-                        </Button>
-                        <ShowUserDetails data={data} />
-                        <p>-----------------------------------</p>
-                        <ShowUsers users={users} />
-                    </>
-                )}
+        <div className="flex flex-col gap-5 bg-neutral-700 rounded-xl p-10">
+            <ShowUserDetails data={data} />
+            <p>-----------------------------------</p>
+            <ShowUsers users={users} />
         </div>
     )
 }
