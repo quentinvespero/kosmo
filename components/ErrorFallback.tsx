adba99aaf52767de8c7908a669bfee5abb05042c
Quentin Vespero <quentinvespero@gmail.com>
2026-04-18 03:14:50 +0200

    ajout icon "broken" on error boundaries

--- components/ErrorFallback.tsx
+++ components/ErrorFallback.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from "next/image"
 import { Card, CardFooter, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 
@@ -20,6 +21,8 @@ const ErrorFallback = ({
         <div className="flex min-h-[50vh] items-center justify-center p-4">
             <Card className="w-full max-w-md">
                 <CardHeader>
+                    <Image src="/logo_light_broken.svg" alt="" width={96} height={96} className="mb-2 mx-auto dark:hidden" />
+                    <Image src="/logo_dark_broken.svg" alt="" width={96} height={96} className="mb-2 mx-auto hidden dark:block" />
                     <CardTitle>{title}</CardTitle>
                     <CardDescription>{message}</CardDescription>
                 </CardHeader>
